aafa6edfcd977a893db775f417cec24750f0d77d
nayisntaround87 <naylasalwahayati@gmail.com>
2025-10-30 09:36:24 +0700

    menambahkan nama, jk dan alamat di button

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -86,5 +86,13 @@ fun FormDataDiri(modifier: Modifier) {
     Button(
         modifier = Modifier.fillMaxWidth(1f),
         // the button is enabled when the user makes a selection
-    )
+        enabled = textAlamat.isNotEmpty(),
+        onClick = {
+            nama=textNama
+            jenis=textJK
+            alamat=textAlamat
+        }
+    ){
+
+    }
 }
