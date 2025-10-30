540c69b643fb90a9233f97d46a7e66a6833281cd
nayisntaround87 <naylasalwahayati@gmail.com>
2025-10-30 09:22:14 +0700

    membuat outlinedtextfield textalamat

--- app/src/main/java/com/example/inputpengguna/TeksEdit.kt
+++ app/src/main/java/com/example/inputpengguna/TeksEdit.kt
@@ -1,6 +1,5 @@
 package com.example.inputpengguna
 
-import android.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,31 +9,31 @@ import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun FormDataDiri(modifier: Modifier
-){
-    //variabel-variabel untuk mengingat nilai masukan dari keyboard
-    var textNama by remember {mutableStateOf("")}
-    var textAlamat by remember { mutableStateOf("")}
-    var textJK by remember {mutableStateOf("")}
+fun FormDataDiri(modifier: Modifier) {
 
-    //variabel-variabel untuk menyimpan data diperoleh dari komponen
-    var nama by remember {mutableStateOf("")}
-    var alamat by remember {mutableStateOf("")}
-    var jenis by remember {mutableStateOf("")}
+    // variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
 
-    val gender:List<string> = listOf("Laki-laki", "Perempuan")
+    // variabel-variabel untuk menyimpan data diperoleh dari komponen
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenis by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.padding(top = 50.dp),
-    verticalArrangement = Arrangement.Top,
-    horizontalAlignment = Alignment.CenterHorizontally) {
+    val gender: List<String> = listOf("Laki-laki", "Perempuan")
+
+    Column(
+        modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         OutlinedTextField(
             value = textNama,
             singleLine = true,
@@ -47,17 +46,29 @@ fun FormDataDiri(modifier: Modifier
         )
         Row {
             gender.forEach { item ->
-                Row(modifier = Modifier.selectable(
-                    selected = textJK == item,
-                    onClick = {
-                        textJK = item
-                    })
-                Text(item)
+                Row(
+                    modifier = Modifier.selectable(
+                        selected = textJK == item,
+                        onClick = {
+                            textJK = item
+                        }
+                    )
+                ) {
+                    Text(item)
+                }
             }
         }
     }
+
     OutlinedTextField(
         value = textAlamat,
-
+        singleLine = true,
+        modifier = Modifier.width(250.dp),
+        label = {Text(text = "Alamat Lengkap")},
+        onValueChange = {
+            textAlamat = it
+        }
     )
+
+
 }
